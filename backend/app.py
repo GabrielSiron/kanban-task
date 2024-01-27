96dfcce40c0fdff4a489cfcbd4214807cb0bf37f
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-27 10:03:18 -0300

    feat: add FlaskMigrate and FlaskCors

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,24 @@
 from apiflask import APIFlask
 from infrastructure.database import db
 from infrastructure.database.entities import User
+from flask_migrate import Migrate
+from flask_cors import CORS
 
 
 def create_app():
 
     app = APIFlask(__name__)
+    migrate = Migrate()
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = "sqlite:///database.db"
 
+    
     with app.app_context():
         db.init_app(app)
+        migrate.init_app(app, db)
         db.create_all()
 
     @app.get("/")
