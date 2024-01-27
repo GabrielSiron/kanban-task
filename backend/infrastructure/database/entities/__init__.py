33d3c8fc0e9bf0ccf8b8d5399b5ea7f8443beb76
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-27 09:23:22 -0300

    feat: create db instance and import models

---
+++ backend/infrastructure/database/entities/__init__.py
@@ -0,0 +1,5 @@
+from infrastructure.database.entities.user import User
+from infrastructure.database.entities.cycle import Cycle
+from infrastructure.database.entities.day import Day
+from infrastructure.database.entities.task import Task
+from infrastructure.database.entities.tag import Tag
