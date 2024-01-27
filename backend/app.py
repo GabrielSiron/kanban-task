38fef87a1b7587a3de17334ebc7272cb4014fc60
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-27 09:22:12 -0300

    feat: config sqlite

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 from apiflask import APIFlask
+from infrastructure.database import db
+from infrastructure.database.entities import User
+
 
 def create_app():
 
     app = APIFlask(__name__)
 
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "sqlite:///database.db"
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
     @app.get("/")
     def home():
-        return "Hello, World!!!"
+        result = User.query.all()
+        return f"Hello, World!!! {str(result)}"
 
     return app
 if __name__ == "__main__":
