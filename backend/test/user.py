c382c477bcfa9f7aebfa23baaa1e6469789a7883
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-28 12:07:41 -0300

    feat: implements base test-case

---
+++ backend/test/user.py
@@ -0,0 +1,10 @@
+from test import BaseTestCase
+
+
+class UserTest(BaseTestCase):
+    def test_get_user_by_id(self):
+        response = self.client.get('/user/1')
+        self.assert200(response)
+        self.assertEqual(response.json, {
+            "name": "Gabriel"
+        })
