6ac26476feea34ba25fd1e3839e26e4bab5c1254
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-28 23:48:58 -0300

    feat: add signup page and input component

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps(['placeholder', 'type', 'color'])
+
+</script>
+
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <div :class="'input ' + color">
+        <input :type="type" :placeholder="placeholder" />
+        <span class="material-symbols-outlined">
+        </span>
+    </div>
+</template>
+
+<style>
+
+.input {
+    border: 1px solid black;
+    border-radius: 4px;
+    display: flex;
+    height: 48px;
+    min-width: 100px;
+}
+
+input {
+    border: none;
+    width: 80%;
+    padding: 10px;
+    display: flex;
+    font-family: 'Inter', sans-serif;
+}
+
+input:focus {
+    outline: none;
+}
+
+.dark-primary {
+    border: 1px solid var(--dark-primary);
+}
+
+</style>
---
+++ frontend/src/pages/signup.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import Input from '../components/Input.vue';
+
+</script>
+<template>
+    
+    <div class="page">
+        <div class="image-container">
+            <h2>Divide your tasks to conquer your results.</h2>
+            <h4>The solution you need.</h4>
+        </div>
+        <div class="form-container">
+            <h1>Welcome</h1>
+            <h4>Good to see you again</h4>
+            <Input class="text-input" placeholder="name" type="text" color="dark-primary"/>
+            <Input class="text-input" placeholder="email" type="text" color="dark-primary"/>
+            <Input class="text-input" placeholder="password" type="password" color="dark-primary"/>
+        </div>
+    </div>
+</template>
+
+<style>
+
+h2, h1 {
+    padding: 90px 40px 0;
+}
+
+h4 {
+    padding-top: 20px;
+}
+
+h2, h4, h1 {
+    text-align: center;
+}
+
+h2, .image-container h4 {
+    color: white;
+}
+
+h1, .form-container h4 {
+    color: #1b1f45;
+    padding-bottom: 20px;
+}
+
+.title, .slogan {
+    display: block;
+    width: 100%;
+}
+
+.page {
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.text-input {
+    margin-bottom: 10px;
+    width: 40%;
+}
+.image-container {
+    position: relative;
+    justify-content: center;
+    text-align: center;
+    background-image: url(../assets/images/img-side.png);
+    background-position-x: center;
+    background-position-y: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+.image-container .fill {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #1b1f458f !important;
+    width: 100%;
+    height: 100%;
+}
+
+.form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+@media screen and (max-width: 30em){
+    .page {
+        display: flex;
+    }
+
+    .page .image-container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        background-position-y: top;
+        height: 100vh;
+    }
+
+    .page .form-container {
+        width: 100%;
+        background: rgb(255,255,255);
+        background: linear-gradient(0deg, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0) 100%); 
+        z-index: 10;
+    }
+    
+    .image-container h2, .image-container h4 {
+        display: none;
+    }
+}
+</style>
