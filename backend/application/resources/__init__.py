1facbea9fbafbfc3493effe04cdebfec99749307
Roberto Luis <130838869+RobertsLuis@users.noreply.github.com>
2024-01-30 19:20:15 -0300

    feat: add signin and login endpoints

--- backend/application/resources/__init__.py
+++ backend/application/resources/__init__.py
@@ -1,1 +1,2 @@
 from application.resources.user import router as user_routes
+from application.resources.auth import router as auth_routes
---
+++ backend/application/resources/auth.py
@@ -0,0 +1,40 @@
+from ...domain.models.auth import check_pw_pattern
+from ...app import app
+from apiflask import APIFlask, APIBlueprint, abort
+import bcrypt
+from infrastructure.database import db
+from infrastructure.database.entities import User
+
+router = APIBlueprint("authentications", __name__)
+
+
+@router.post("/SignIn")
+@input("name", str)
+@input("email", str)
+@input("password", str)
+def signin(name, email, password):
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return {"message": "Email já cadastrado", "code": 400}
+    elif not (check_pw_pattern(password)):
+        return {"message": "Senha fraca", "code": 400}
+    else:
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        new_user = User(name=name, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return {"message": "Usuário cadastrado com sucesso", "code": 201}
+
+
+@router.post("/Login")
+@input("email", str)
+@input("password", str)
+def login(email, password):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+            return {"message": "Login bem-sucedido", "code": 200}
+        else:
+            return {"message": "Senha incorreta", "code": 401}
+    else:
+        return {"message": "Usuário não encontrado", "code": 401}
