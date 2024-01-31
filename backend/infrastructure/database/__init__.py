a7a7e0e9eecbde708308bb2534e550c9c0e38e7b
gabriel.siron@bloxs.com.br <gabriel.siron@bloxs.com.br>
2024-01-31 02:35:16 -0300

    feat: overwrite commit function to add try-except-finally structure

--- backend/infrastructure/database/__init__.py
+++ backend/infrastructure/database/__init__.py
@@ -1,3 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session
+from apiflask import abort
 
+class Session_(scoped_session):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+    
+    def commit(self, *args, **kwargs):
+        success = True
+        
+        try:
+            super().commit(*args, **kwargs)
+        except:
+            self.rollback(*args, **kwargs)
+            success = False
+        finally:
+            self.close()
+
+        return success
+        
 db = SQLAlchemy()
+db.session = Session_()
+            
